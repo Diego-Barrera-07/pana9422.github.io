d0f983e8d8268607c1fe9fea27222aadb4282134
Miguel J Medina <pana9422@gmail.com>
2022-03-19 23:44:25 -0500

    Dropdowns de primerl nivel, terminado

--- src/app.js
+++ src/app.js
@@ -1,35 +1,14 @@
 let sidebar = document.getElementById("sidebar");
 let sidebar_button = document.getElementById("sidebar-button");
 let overlay = document.getElementById("overlay");
-
-
-let sidebar_dropdown = document.querySelectorAll(".sidebar .sidebar__item[data-type=dropdown]");
-
-sidebar_dropdown.forEach(dropdown => {
-  dropdown.addEventListener("click", function(e){
-
-    this.querySelector(".sidebar__icon-right").classList.add('rotate-90');
-
-  })
-});
-
-// sidebar_dropdown.addEventListener("click", function(e){
-//   console.log(this.getAttribute('data-type'));
-// })
-
-
-
-
-
-if (window.innerWidth >= 1020 ) {
+if (window.innerWidth >= 1020) {
   sidebar.classList.add("sidebar--relative-collapsed");
 }
-overlay.addEventListener("click", function(e) {
+overlay.addEventListener("click", function (e) {
   sidebar.classList.remove("sidebar--absolute");
   overlay.classList.remove("d-block");
 });
-
-window.addEventListener("resize", function(e) {
+window.addEventListener("resize", function (e) {
   let window_width = window.innerWidth;
 
   if (window_width < 1020) {
@@ -44,8 +23,7 @@ window.addEventListener("resize", function(e) {
     sidebar.classList.remove("sidebar--relative-icon");
   }
 });
-
-sidebar_button.addEventListener("click", function(e) {
+sidebar_button.addEventListener("click", function (e) {
   let window_width = window.innerWidth;
 
   if (window_width < 1020) {
@@ -57,17 +35,38 @@ sidebar_button.addEventListener("click", function(e) {
 
     sidebar_view = false;
   } else if (window_width >= 1020) {
-
-    if ( sidebar.className.match("sidebar--relative-collapsed") ) {
+    if (sidebar.className.match("sidebar--relative-collapsed")) {
       sidebar.className = "sidebar sidebar--relative-icon";
     } else {
       sidebar.className = "sidebar sidebar--relative-collapsed";
     }
-
-
-    // sidebar.classList.remove("sidebar--absolute");
-    // sidebar.classList.toggle("sidebar--relative-collapsed");
-    // sidebar.classList.toggle("sidebar--relative-icon");
   }
 });
 
+
+
+
+let sidebar_dropdowns_links = document.querySelectorAll(".sidebar__item[data-type=dropdown] > .sidebar__link");
+sidebar_dropdowns_links.forEach( (dropdown_link) => dropdownOpen(dropdown_link) );
+
+function dropdownOpen( dropdown_link ) {
+
+  dropdown_link.addEventListener("click", (e) => {
+    e.preventDefault();
+    dropdownsClosed(dropdown_link);
+    
+    dropdown_link.parentNode.classList.toggle("sidebar__item--open");
+    dropdown_link.querySelector(".sidebar__icon-right").classList.toggle("rotate-90");
+
+  });
+}
+function dropdownsClosed( dropdown_link ){
+  let sidebar_item = document.querySelectorAll(".sidebar__item[data-type=dropdown]");
+
+  sidebar_item.forEach(element => {
+    if(dropdown_link.parentNode !== element){
+      element.classList.remove("sidebar__item--open");
+      element.querySelector(".sidebar__link > .sidebar__icon-right").classList.remove("rotate-90");
+    }
+  });
+}
